289855199c12a48bbb0a295ccff38d6870d536fa
DeniArya <91365553+DeniArya@users.noreply.github.com>
2021-09-25 17:24:14 +0700

    menambahkan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Deni Arya Prasetya
+              Saya inggal di daerah Bekasi
+              Olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
